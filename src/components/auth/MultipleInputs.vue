bf0d5cd30364dafc418563e5e8e2b5addb3a9c99
pgilkrog <33147105+pgilkrog@users.noreply.github.com>
2023-03-13 20:26:33 +0100

    minor cleanup

--- src/components/auth/MultipleInputs.vue
+++ src/components/auth/MultipleInputs.vue
@@ -1,27 +1,67 @@
 <template lang="pug">
 label(for="name") Full name:
-input.bg-shadow-inner(type="text" :value="name" @input="$emit('update:name', $event.target.value)" id="name")
+input.bg-shadow-inner(
+  type="text" 
+  :value="name" 
+  @input="$emit('update:name', $event.target.value)" 
+  id="name"
+)
 
-label(for="address-line") Address:
-input.bg-shadow-inner(type="text" :value="addressLine" @input="$emit('update:addressLine', $event.target.value)" id="address-line")
+label.mt-4(for="address-line") Address:
+input.bg-shadow-inner(
+  type="text" 
+  :value="addressLine" 
+  @input="$emit('update:addressLine', $event.target.value)" 
+  id="address-line"
+)
 
-label(for="street-house-number") Street nr:
-input.bg-shadow-inner(type="text" :value="streetNumber" @input="$emit('update:streetNumber', $event.target.value)" id="street-house-number")
+label.mt-4(for="street-house-number") Street nr:
+input.bg-shadow-inner(
+  type="text" 
+  :value="streetNumber" 
+  @input="$emit('update:streetNumber', $event.target.value)" 
+  id="street-house-number"
+)
 
-label(for="town-city") Town/City:
-input.bg-shadow-inner(type="text" :value="town" @input="$emit('update:town', $event.target.value)" id="town-city")
+label.mt-4(for="town-city") Town/City:
+input.bg-shadow-inner(
+  type="text" 
+  :value="town" 
+  @input="$emit('update:town', $event.target.value)" 
+  id="town-city"
+)
 
-label(for="country") Country:
-input.bg-shadow-inner(type="text" :value="country" @input="$emit('update:country', $event.target.value)" id="country")
+label.mt-4(for="country") Country:
+input.bg-shadow-inner(
+  type="text" 
+  :value="country" 
+  @input="$emit('update:country', $event.target.value)" 
+  id="country"
+)
 
-label(for="postcode") Postcode:
-input.bg-shadow-inner(type="text" :value="postcode" @input="$emit('update:postcode', $event.target.value)" id="postcode")
+label.mt-4(for="postcode") Postcode:
+input.bg-shadow-inner(
+  type="text" 
+  :value="postcode" 
+  @input="$emit('update:postcode', $event.target.value)" 
+  id="postcode"
+)
 
-label(for="phone-number") Phone number:
-input.bg-shadow-inner(type="text" :value="phoneNumber" @input="$emit('update:phoneNumber', $event.target.value)" id="phone-number")
+label.mt-4(for="phone-number") Phone number:
+input.bg-shadow-inner(
+  type="text" 
+  :value="phoneNumber" 
+  @input="$emit('update:phoneNumber', $event.target.value)" 
+  id="phone-number"
+)
 
-label(for="is-home-address") Is home address?:
-input.bg-shadow-inner(type="checkbox" :checked="homeAddress" @change="$emit('update:homeAddress', $event.target.checked)" id="is-home-address")
+label.mt-4(for="is-home-address") Is home address?:
+input.bg-shadow-inner(
+  type="checkbox" 
+  :checked="homeAddress" 
+  @change="$emit('update:homeAddress', $event.target.checked)" 
+  id="is-home-address"
+)
 </template>
 
 <script lang="ts">
