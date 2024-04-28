9a7ca243a9f8e1e0e18d263f3b96637f6cf31f7b
pgilkrog <33147105+pgilkrog@users.noreply.github.com>
2024-04-28 21:12:34 +0200

    removed backend from this project

--- Backend/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export { Program } from './Program'
-export { User } from './User'
-export { UserSettings } from './UserSettings'
-export { Error } from './Error'
-export { File } from './File'
-export { Painting } from './Painting'
